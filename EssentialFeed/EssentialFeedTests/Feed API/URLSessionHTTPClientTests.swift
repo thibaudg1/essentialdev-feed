cc9397aae28dae52fcbc77493b9a8f5b5e86e90e
thibaudgourlay <148068363+thibaudgourlay@users.noreply.github.com>
2025-09-01 20:57:46 +0300

    resumes data task on creation

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -8,9 +8,7 @@ class URLSessionHTTPClient {
     }
     
     func get(from url: URL) {
-        session.dataTask(with: url) { _, _, _ in
-            
-        }
+        session.dataTask(with: url) { _, _, _ in }.resume()
     }
 }
 class UURLSessionHTTPClientTests: XCTestCase {
@@ -25,22 +23,47 @@ class UURLSessionHTTPClientTests: XCTestCase {
         XCTAssertEqual(session.receivedURLs, [url])
     }
     
+    func test_getFromURL_resumesDataTaskWithURL() {
+        let url = URL(string: "https://www.google.com")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        session.stub(url: url, task: task)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(task.resumeCallCount, 1)
+    }
+    
     // MARK: - Helpers
     
     private class URLSessionSpy: URLSession {
         var receivedURLs: [URL] = []
+        private var stubs: [URL: URLSessionDataTask] = [:]
+        
+        func stub(url: URL, task: URLSessionDataTask) {
+            stubs[url] = task
+        }
         
         override func dataTask(
             with url: URL,
             completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
         ) -> URLSessionDataTask {
             receivedURLs.append(url)
-            return FakeURLSessionDataTask()
+            return stubs[url] ?? FakeURLSessionDataTask()
         }
     }
     
     private class FakeURLSessionDataTask: URLSessionDataTask {
+        override func resume() {}
+    }
+    
+    private class URLSessionDataTaskSpy : URLSessionDataTask {
+        var resumeCallCount: Int = 0
         
+        override func resume() {
+            resumeCallCount += 1
+        }
     }
 }
 
