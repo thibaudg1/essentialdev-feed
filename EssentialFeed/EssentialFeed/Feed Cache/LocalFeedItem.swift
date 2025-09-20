3429efe5e7854902fd93ff896d644575e12a4d50
thibaudg1 <96113614+thibaudg1@users.noreply.github.com>
2025-09-20 09:31:26 +0300

    Move LocalFeedItem to its own file

---
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,15 @@
+ import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
