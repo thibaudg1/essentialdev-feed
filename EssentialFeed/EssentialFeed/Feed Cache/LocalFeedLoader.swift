3dd7204fa9d1a950b71f5ad9c9215f59d299d5aa
thibaudg1 <96113614+thibaudg1@users.noreply.github.com>
2025-09-20 17:43:29 +0300

    Validate cache command does not delete invalid cache after instance has been deallocated

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -46,10 +46,13 @@ public final class LocalFeedLoader {
     }
     
     public func validateCache() {
-        store.retrieve { [unowned self] result in
+        store.retrieve { [weak self] result in
+            guard let self else { return }
+            
             switch result {
             case .failure:
                 self.store.deleteCachedFeed { _ in }
+                
             case let .found(_, timestamp) where !self.validate(timestamp):
                 self.store.deleteCachedFeed { _ in }
                 
